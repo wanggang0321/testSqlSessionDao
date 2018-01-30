717f310ce035d057bff5893e9227b8625032fc19
wanggang0321 <578620382@qq.com>
2018-01-30 17:49:46 +0800

    1.增加地址查询方法

--- src/main/java/com/test/address/service/IAddressService.java
+++ src/main/java/com/test/address/service/IAddressService.java
@@ -1,5 +1,12 @@
 package com.test.address.service;
 
 public interface IAddressService {
+	
+	/**
+	 * 随机生成地区
+	 * @return
+	 * @throws Exception
+	 */
+	public int randomAreaCode() throws Exception;
 
 }
--- src/main/java/com/test/address/service/impl/AddressServiceImpl.java
+++ src/main/java/com/test/address/service/impl/AddressServiceImpl.java
@@ -1,7 +1,31 @@
 package com.test.address.service.impl;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.test.address.dao.AddressDao;
 import com.test.address.service.IAddressService;
 
+@Service
 public class AddressServiceImpl implements IAddressService {
-
+	
+	@Autowired
+	private AddressDao addressDao;
+	
+	public int randomAreaCode() throws Exception {  
+        int index = (int) (Math.random() * addressDao.areaCode.size());  
+        Collection<Integer> values = addressDao.areaCode.values();  
+        Iterator<Integer> it = values.iterator();  
+        int i = 0;  
+        int code = 0;  
+        while (i < index && it.hasNext()) {  
+            i++;  
+            code = it.next();  
+        }  
+        return code;  
+    }
+	
 }
