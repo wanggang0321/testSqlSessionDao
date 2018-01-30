862d855dd6a057f5169c71733e8ce818386ac3dc
wanggang0321 <578620382@qq.com>
2018-01-30 17:59:02 +0800

    1.去掉junit单元测试

--- src/main/java/com/test/address/controller/AddressController.java
+++ src/main/java/com/test/address/controller/AddressController.java
@@ -1,6 +1,5 @@
 package com.test.address.controller;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
