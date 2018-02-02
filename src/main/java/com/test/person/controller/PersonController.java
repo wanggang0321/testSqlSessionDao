7b041829721c3d83b2afdcec4aebe98b6a6a1ad2
wanggang0321 <578620382@qq.com>
2018-02-02 11:21:37 +0800

    1.生成个人信息

---
+++ src/main/java/com/test/person/controller/PersonController.java
@@ -0,0 +1,32 @@
+package com.test.person.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.test.person.entity.Person;
+import com.test.person.service.IPersonService;
+
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private IPersonService personService;
+	
+	@RequestMapping(value="/create", method={RequestMethod.GET, RequestMethod.POST})
+	public void createPerson() {
+		try {
+			Person person = personService.createPerson();
+			logger.info(person.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
--- src/main/java/com/test/person/service/impl/PersonServiceImpl.java
+++ src/main/java/com/test/person/service/impl/PersonServiceImpl.java
@@ -3,10 +3,12 @@ package com.test.person.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.test.address.service.IAddressService;
 import com.test.idcard.service.IdCardService;
 import com.test.name.service.INameService;
 import com.test.person.entity.Person;
 import com.test.person.service.IPersonService;
+import com.test.phone.service.IPhoneService;
 
 @Service
 public class PersonServiceImpl implements IPersonService {
@@ -15,22 +17,32 @@ public class PersonServiceImpl implements IPersonService {
 	private IdCardService idCardService;
 	@Autowired
 	private INameService nameService;
+	@Autowired
+	private IPhoneService phoneService;
+	@Autowired
+	private IAddressService addressService;
 	
 	public Person createPerson() throws Exception {
 		
+		Person person = new Person();
+		
 		//姓名
 		String chineseName = nameService.generate();
+		person.setName(chineseName);
 		
 		//身份证号
 		String idCardNumber = idCardService.generate();
+		person.setIdCardNumber(idCardNumber);
 		
 		//性别
 		
 		//所属地区
 		
 		//手机号
+		String tel = phoneService.getTel();
+		person.setCellphone(tel);
 		
-		return null;
+		return person;
 	}
 	
 }
