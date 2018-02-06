b987d24028113f6e88433598caeb7445f225c65c
wanggang0321 <578620382@qq.com>
2018-02-06 10:25:39 +0800

    1.MyBatis测试程序

--- src/main/java/com/ppdtbb/mybatis/MyBatisFirst.java
+++ src/main/java/com/ppdtbb/mybatis/MyBatisFirst.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.ppdtbb.user.pojo.User;
+
 public class MyBatisFirst {
 	
 	public SqlSession getSqlSession() throws Exception {
@@ -24,6 +26,23 @@ public class MyBatisFirst {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		
 		return sqlSession;
+	}
+	
+	public void findUserById(Integer userId) throws Exception {
+		
+		//获得SqlSession
+		SqlSession session = getSqlSession();
+		
+		/*
+		 * 通过SqlSession操作数据库
+		 * 第一个参数：映射文件中statement的id = namespace + statement的id
+		 * 第二个参数：指定和映射文件中parameterType类型所匹配的参数
+		 */
+		//selectOne查询出一条记录进行映射
+		User user = session.selectOne("", userId);
+		
+		//释放资源
+		session.close();
 	}
 	
 }
