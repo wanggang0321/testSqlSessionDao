a275ed7e9e76d51acd9e32a4813cad8d315a51a9
wanggang0321 <578620382@qq.com>
2018-02-06 11:45:51 +0800

    1.jdbc查询并添加log4j日志 2.pom添加mysql驱动jar依赖

--- src/main/java/com/ppdtbb/jdbc/dao/RoleDao.java
+++ src/main/java/com/ppdtbb/jdbc/dao/RoleDao.java
@@ -5,29 +5,57 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ppdtbb.jdbc.model.Role;
 import com.ppdtbb.jdbc.util.DbUtil;
 
 public class RoleDao {
 	
-	public Role get() throws SQLException {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Test
+	public void testJdbc() {
+		try {
+			Role role = get(2);
+			System.out.println(role);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Role get(Integer id) throws SQLException {
+		
+		Role role = null;
 		
 		//获取连接
 		Connection conn = DbUtil.getConnection();
 		
 		//sql
-		String sql = "select * from role where roldId=?";
+		String sql = "select * from role where roleId=?";
 		
 		//预编译sql，减少sql执行
 		PreparedStatement ptmt = conn.prepareStatement(sql);
 		
 		//传参
-		ptmt.setInt(0, 1);
+		ptmt.setString(1, "2");
 		
 		//执行
 		ResultSet rs = ptmt.executeQuery();
 		
-		return null;
+		while(rs.next()) {
+			role = new Role();
+			String roleId = rs.getString("roleId");
+			String roleName = rs.getString("roleName");
+			logger.info("roleId is {}, roleName is {}", roleId, roleName);
+			
+			role.setRoleID(roleId);
+			role.setRoleName(roleName);
+		}
+		
+		return role;
 	}
 	
 }
