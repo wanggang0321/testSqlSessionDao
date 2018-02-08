716444e03f77a936d5a01ca6deab9631b6b46429
wanggang0321 <578620382@qq.com>
2018-02-08 10:20:31 +0800

    1.MyBatis的Mapper代理方法开发dao

---
+++ src/main/java/com/ppdtbb/mybatis/UserMapperTest.java
@@ -0,0 +1,59 @@
+package com.ppdtbb.mybatis;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ppdtbb.mybatis.mapper.UserMapper;
+import com.ppdtbb.user.pojo.User;
+
+public class UserMapperTest {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private SqlSessionFactory sqlSessionFactory;
+	
+	/**
+	 * 创建SqlSessionFactory
+	 */
+	@Before
+	public void createSqlSessionFactory() throws Exception {
+		//配置文件
+		String file = "mybatis/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(file);
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	@Test
+	public void testFindUserById() throws Exception {
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		//创建UserMapper对象，MyBatis自动生成mapper代理对象
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User user = userMapper.findUserById(1);
+		logger.info("user info is :" + user.getUsername() + user.getBirthday() + user.getAddress());
+		sqlSession.close();
+	}
+	
+	@Test
+	public void testFindUserByName() throws Exception {
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		//创建UserMapper对象，MyBatis自动生成mapper代理对象
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		List<User> userList = userMapper.findUserByName("国");
+		for(User user : userList) {
+			logger.info("user info is :" + user.getUsername() + user.getBirthday() + user.getAddress());
+		}
+		sqlSession.close();
+	}
+	
+}
