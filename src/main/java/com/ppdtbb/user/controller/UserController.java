fad64f900bde09cc70335f92cfc791eb8f49da9f
wanggang0321 <578620382@qq.com>
2018-03-02 17:31:11 +0800

    1.添加拦截器

---
+++ src/main/java/com/ppdtbb/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.ppdtbb.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/global")
+public class UserController {
+
+}
